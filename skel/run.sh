61ade35bbbf7303d5c2e14bb97b70554e6f4755f
Paul Furley <paul@paulfurley.com>
2013-06-03 15:55:53 +0100

    Updated run.sh to look for main.py and main.sh

--- skel/run.sh
+++ skel/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This script can be run regularly - it will kick off tool/main.py|sh and
+# send the output to ~/log
+
+
 DATE_NOW=$(date +%Y-%m-%d_%H-%M-%S)
 STATUS_URL='https://beta.scraperwiki.com/api/status'
 LOG_DIR=~/log
@@ -9,20 +13,20 @@ LOG_FILE=${LOG_DIR}/${DATE_NOW}.log
 
 ln -sf ${LOG_FILE} ${LOG_DIR}/latest
 
-for command in 01_download.py 02_process.py
+for command in tool/main.py tool/main.sh
 do
-    # Run the command specified on the command line
-    run-one ${command} >> ${LOG_FILE} 2>&1
-    RETCODE=$?
-    if [ ${RETCODE} != 0 ]; then
-        echo "$@ exited with code: ${RETCODE}"
-        cat ${LOG_FILE}
-        curl --data "type=error" ${STATUS_URL} > /dev/null 2>&1
-        exit
+    if [ -f "$command" ]; then
+        run-one ${command} >> ${LOG_FILE} 2>&1
+        RETCODE=$?
+        if [ ${RETCODE} != 0 ]; then
+            echo "$@ exited with code: ${RETCODE}"
+            cat ${LOG_FILE}
+            curl --data "type=error" ${STATUS_URL} > /dev/null 2>&1
+            exit
+        fi
     fi
 done
 
-
-# delete logs older than a week
-find ${LOG_DIR} -type f -iname '*.log' -mtime +7 -delete
+# delete logs older than a month
+find ${LOG_DIR} -type f -iname '*.log' -mtime +30 -delete
 
